45b5c89249bf52219f306356af2a62b73932b03f
heymynameisrob <robhough180@gmail.com>
2025-01-03 15:09:31 +0000

    Clickable public link

--- src/components/docs/docs-shared.tsx
+++ src/components/docs/docs-shared.tsx
@@ -1,14 +1,27 @@
+import { toast } from "sonner";
 import { GlobeAltIcon } from "@heroicons/react/16/solid";
 
 import { useUser } from "@/queries/user";
+import { createSlug } from "@/lib/utils";
 
 export function DocsShared() {
   const { user } = useUser();
 
+  const handleCopyLink = () => {
+    if (!user) return;
+    navigator.clipboard.writeText(
+      `https://pilcrow.xyz/s/${createSlug(user.name!)}-${user.publicId}`,
+    );
+    toast.success("Copied link to clipboard");
+  };
+
   if (!user?.isPublic) return null;
 
   return (
-    <div className="fixed bottom-0 right-0 flex items-center gap-2 px-1.5 py-1 bg-cyan-400 text-black z-50 text-xs uppercase font-semibold tracking-wide rounded-tl-lg">
+    <div
+      onClick={handleCopyLink}
+      className="fixed bottom-0 right-0 flex items-center gap-2 px-1.5 py-1 bg-cyan-400 text-black z-50 text-xs uppercase font-semibold tracking-wide rounded-tl-lg cursor-pointer"
+    >
       <GlobeAltIcon className="w-4 h-4 opacity-70" />
       Public
     </div>
